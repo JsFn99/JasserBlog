754588731cc3fddeafbfd868851a2e81457cbbdb
JsFn99 <fnine2015@gmail.com>
2024-08-01 16:34:59 +0100

    yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const LIVE_URL = 'https://jasserblog.github.io/'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: LIVE_URL,
+    integrations: [tailwind()]
 });
